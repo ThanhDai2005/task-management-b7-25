3cae8db2a08bf290b79c2a570240146573102317
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-10-13 18:08:11 +0700

    5. Lưu id của user đã tạo task

--- api/v1/middlewares/auth.middleware.js
+++ api/v1/middlewares/auth.middleware.js
@@ -3,7 +3,6 @@ const User = require("../models/user.model");
 module.exports.requireAuth = async (req, res, next) => {
   if (req.headers.authorization) {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
 
     const user = await User.findOne({
       token: token,
@@ -18,7 +17,7 @@ module.exports.requireAuth = async (req, res, next) => {
       return;
     }
 
-    req.user = user;
+    res.locals.user = user;
 
     next();
   } else {
